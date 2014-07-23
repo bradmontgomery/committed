e6452be1580f5764b911dc8d0eaef41073c90a8b
Brad Montgomery <brad@bradmontgomery.net>
2014-07-22 21:31:03 -0500

    things to come

--- committed/main.py
+++ committed/main.py
@@ -1,1 +1,11 @@
-# TODO: ...
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<html><body>Weeee</body></html>"
+
+
+if __name__ == "__main__":
+    app.run()
